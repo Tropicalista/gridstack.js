b9ca4a3bc5c7c77784637171cd1bcc8d1adff88d
Francesco <francescopepe@email.it>
2015-07-01 06:19:59 +0200

    Removed fontawesome

--- src/angular/gridstack.js
+++ src/angular/gridstack.js
@@ -7,11 +7,17 @@
       return {
           restrict: "A",
           controller: 'GridstackController',
-          link: function (scope, element, attrs) {
+          link: function (scope, element, attrs, ngModel) {
 
               scope.init(element, {});
+
+              element.on('dragstop', function (e, items) {
+                console.log("dragstop")
+              })
               element.on('change', function (e, items) {
-                  var res = _.map($('.grid-stack .grid-stack-item:visible'), function (el) {
+                  console.log('change')
+
+                  var res1 = _.map($('.grid-stack .grid-stack-item:visible'), function (el) {
                       el = $(el);
                       var node = el.data('_gridstack_node');
                       return {
@@ -21,8 +27,21 @@
                           width: node.width,
                           height: node.height
                       };
-                  }); 
-                  scope.serialize(res);
+                  });
+                  //console.log('items', items)
+                  var res2 = _.map(items, function(item){
+                      //console.log(item)
+                      return {
+                          x: item.x,
+                          y: item.y,
+                          width: item.width,
+                          height: item.height
+                      }
+                  });
+
+                    scope.widgets = res1;
+                    scope.$apply();
+
               });
 
           }
@@ -36,8 +55,18 @@
           link: function (scope, element, attrs, controller) {
 
               var widget = controller.addItem(element, attrs.gsX,attrs.gsY,attrs.gsWidth,attrs.gsHeight);
+                widget.bind('change', function() {
+                  console.log('change widget', element)
+                });
+
+                widget.bind('$destroy', function() {
+                  console.log('destroy', element)
+                  //controller.removeItem(element);
+                });
+
 
           }
+
       };
   }]);
 
